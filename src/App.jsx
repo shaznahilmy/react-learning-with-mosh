9094d06b35ff0775d52b3ed62e75988c7aef2f11
shaznahilmy <shazhilmy@gmail.com>
2024-08-19 08:40:26 +0530

    passing functions as props

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ var heading = "Cities";
 // var items = ["France", "Korea", "Japan", "England", "USA"];
 // var heading = "Countries";
 
+const handleSelect = (item) => {
+  console.log("Selected item: ",item);
+
+};
+
 function App(){
-  return <div><ListGroup items={items} heading={heading}/></div>
+  return <div><ListGroup items={items} heading={heading} onSelect={handleSelect}/></div>
 }
 export default App;
--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -4,21 +4,21 @@ import { useState } from "react";
  * @typedef {Object} Props
  * @property {string[]} items - The array of strings.
  * @property {string} heading - The heading string.
+ * @property {function(string): void} onSelect - The function to call when an item is selected.
  */
 
 /**
  * @param {Props} props
  */
 
-function ListGroup({items, heading}) {
-  
+function ListGroup({ items, heading, onSelect }) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>{heading}</h1>
 
       {items.length === 0 && <p>No Item Found!</p>}
-      
+
       <ul className="list-group">
         {/*key helps to indentify each item so react can keep track of it */}
         {items.map((item, index) => (
@@ -29,7 +29,10 @@ function ListGroup({items, heading}) {
                 : "list-group-item"
             }
             key={item}
-            onClick={() => {setSelectedIndex(index),console.log(item)}}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelect(item);
+            }}
           >
             {item}
           </li>
